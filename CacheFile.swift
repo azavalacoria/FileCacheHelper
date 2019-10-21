5b110dfef5b5526a6381489a9d8da74f4b48aa1e
Emmanuel <emmanuelfloresisc@gmail.com>
2019-10-21 18:13:46 -0500

    Initial Commit

---
+++ CacheFile.swift
@@ -0,0 +1,20 @@
+//
+//  CacheFile.swift
+//  FileCacheHElper
+//
+//  Created by Emmanuel Eduardo Flores Chab on 10/21/19.
+//  Copyright © 2019 Sahfer. All rights reserved.
+//
+
+import Foundation
+
+class CacheFile {
+    
+    func createTempFile() {
+        let tempDirectory = URL(fileURLWithPath: NSTemporaryDirectory(),
+                                isDirectory: true);
+        
+        
+        
+    }
+}
